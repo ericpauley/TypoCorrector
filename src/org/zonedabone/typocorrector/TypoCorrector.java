ba1257df0a1bec6d057c62a9c86c191f5d6c64bc
zonedabone <zonedabone@gmail.com>
2012-08-16 19:03:25 -0400

    Fetch replacements from wikipedia page.
    I don't want to have to update that list every 5 minutes.

--- src/org/zonedabone/typocorrector/TypoCorrector.java
+++ src/org/zonedabone/typocorrector/TypoCorrector.java
@@ -5,8 +5,10 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,15 +22,19 @@ public class TypoCorrector extends JavaPlugin implements Listener {
 	Map<String, String> replacements;
 
 	public void onEnable() {
-		loadTypos();
+		new Thread(){
+			public void run(){
+				loadTypos();
+			}
+		}.start();
 		this.getServer().getPluginManager().registerEvents(this, this);
 	}
 
 	@EventHandler
-	public void onPlayerChat(AsyncPlayerChatEvent e){
-		if(e.getPlayer().hasPermission("typo.correct")){
+	public void onPlayerChat(AsyncPlayerChatEvent e) {
+		if (e.getPlayer().hasPermission("typo.correct")) {
 			String message = e.getMessage();
-			for(Map.Entry<String, String> typo:replacements.entrySet()){
+			for (Map.Entry<String, String> typo : replacements.entrySet()) {
 				message = correct(message, typo.getKey(), typo.getValue());
 			}
 			e.setMessage(message);
@@ -36,16 +42,18 @@ public class TypoCorrector extends JavaPlugin implements Listener {
 	}
 
 	private String correct(String text, String typo, String replacement) {
-		Pattern p = Pattern.compile("\\b"+typo+"\\b", Pattern.CASE_INSENSITIVE);
+		Pattern p = Pattern.compile("\\b" + typo + "\\b",
+				Pattern.CASE_INSENSITIVE);
 		Matcher m = p.matcher(text);
 
 		StringBuffer sb = new StringBuffer();
 
 		while (m.find()) {
 			String replace;
-			if(Character.isUpperCase(m.group().toCharArray()[0])){
-				replace = Character.toUpperCase(m.group().toCharArray()[0])+replacement.substring(1);
-			}else{
+			if (Character.isUpperCase(m.group().toCharArray()[0])) {
+				replace = Character.toUpperCase(m.group().toCharArray()[0])
+						+ replacement.substring(1);
+			} else {
 				replace = replacement;
 			}
 			m.appendReplacement(sb, Matcher.quoteReplacement(replace));
@@ -56,16 +64,34 @@ public class TypoCorrector extends JavaPlugin implements Listener {
 	}
 
 	private void loadTypos() {
-		replacements = new HashMap<String, String>();
-		InputStream istream = this.getResource("typos.txt");
+		replacements = new ConcurrentHashMap<String, String>();
+		InputStream istream;
+		try {
+			istream = new URL(
+					"http://en.wikipedia.org/w/api.php?format=xml&action=query&titles=Wikipedia:Lists_of_common_misspellings/For_machines&prop=revisions&rvprop=content")
+					.openStream();
+		} catch (MalformedURLException e1) {
+			e1.printStackTrace();
+			return;
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			return;
+		}
 		DataInputStream in = new DataInputStream(istream);
 		BufferedReader br = new BufferedReader(new InputStreamReader(in));
 		String strLine;
 		try {
+			long loaded = 0;
 			while ((strLine = br.readLine()) != null) {
-				String[] parts = strLine.split("->");
-				replacements.put(parts[0], parts[1].split(", ")[0]);
+				if (strLine.contains("-&gt;")) {
+					strLine = strLine.trim();
+					String[] parts = strLine.split("-&gt;");
+					replacements.put(parts[0], parts[1].split(", ")[0]);
+					loaded++;
+				}
+
 			}
+			getLogger().info("Loaded " + loaded + " replacements.");
 			in.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
